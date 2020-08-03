049ba2ac03e44e1175b66178ef4ae1e1cbef5aa1
Jakeharper6 <398305153@qq.com>
2020-08-03 02:12:24 +0100

    update

---
+++ note/c-practice/2/p2-3.c
@@ -0,0 +1,16 @@
+/*编写一段程序，像下面那样显示出读取的实数的值
+请输入一个实数:57.3
+你输入的是57.300000*/
+
+#include<stdio.h>
+
+int main(void){
+    
+    double x;
+
+    printf("请你输入一个实数:");scanf("%lf",&x);
+
+    printf("%f",x);
+
+    return 0;
+}
