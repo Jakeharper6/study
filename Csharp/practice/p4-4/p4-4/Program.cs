b3b0d7eff58ea11f8cb306098a14b96c61563d0b
Jakeharper6 <398305153@qq.com>
2020-08-20 13:14:50 +0100

    C#prac

--- Csharp/practice/p4-4/p4-4/Program.cs
+++ Csharp/practice/p4-4/p4-4/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
 
 namespace p4_4
 {
@@ -10,6 +12,28 @@ namespace p4_4
     {
         static void Main(string[] args)
         {
+            double numa;
+            double numb;
+
+            do
+            {
+                WriteLine("Enter two numbers below:\n");
+                
+                WriteLine("First number:\n");
+                numa = ToDouble(ReadLine());
+                WriteLine("\n");
+
+                WriteLine("Second number:\n");
+                numb = ToDouble(ReadLine());
+                WriteLine("\n");
+
+                if (numa > 10 && numb > 10)
+                    WriteLine("Both of the numbers are more than 10.");
+            } while (numa > 10 && numb > 10);
+
+            WriteLine($"Your entering numbers are {numa} and {numb}");
+
+            ReadKey();
         }
     }
 }
