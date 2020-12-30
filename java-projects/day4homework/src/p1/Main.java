a32234849417a1b4f938b57fab5aef89d1b93f7b
Jakeharper6 <398305153@qq.com>
2020-12-30 12:58:07 +0000

    update

--- java-projects/day4homework/src/p1/Main.java
+++ java-projects/day4homework/src/p1/Main.java
@@ -1,6 +1,5 @@
 package p1;
 
-import java.util.Scanner;
 
 //根据以下数据编写员工类，并创建对象
 //张三，月薪15000，id=1110
@@ -8,7 +7,16 @@ import java.util.Scanner;
 
 
 public class Main {
-    Scanner scanner = new Scanner(System.in);
+
+    String name;
+    int salary;
+    String identity;
+
+    public Main(String name, int salary, String identity) {
+        this.name = name;
+        this.salary = salary;
+        this.identity = identity;
+    }
 
     /**
      * 将员工的月薪涨幅指定百分比
@@ -16,9 +24,7 @@ public class Main {
      * @param percent 传入一个百分比的值
      */
     public void raiseSalary(double percent) {
-        System.out.println("请输入涨幅程度：");
-        double raiseDegree = scanner.nextDouble();
-        System.out.println(raiseDegree);
+        
     }
 
     /**
@@ -27,9 +33,7 @@ public class Main {
      * @param percent 传入一个百分比的值
      */
     public void cutSalary(double percent) {
-        System.out.println("请输入降幅程度：");
-        double cutDegree = scanner.nextDouble();
-        System.out.println(cutDegree);
+
     }
 
     /**
@@ -37,22 +41,15 @@ public class Main {
      *
      * @return 张三，月薪：xxx, id:xxx
      */
-//    public String getInfo() {
-//
-//
+    public String getInfo() {
+        return "";
+    }
 
     public static void main(String[] args) {
-        Member myMember1 = new Member();
-        myMember1.name = "张三";
-        myMember1.salary = 15000;
-        myMember1.identity = 1110;
+        Main m1 = new Main("张三",15000,"1110");
+        Main m2 = new Main("李四",17000,"1111");
 
-        Member myMember2 = new Member();
-        myMember2.name = "李四";
-        myMember2.salary = 17000;
-        myMember2.identity = 1111;
-
-        System.out.println(myMember1.name + ",月薪" + myMember1.salary + ",id=" + myMember1.identity);
-        System.out.println(myMember2.name + ",月薪" + myMember2.salary + ",id=" + myMember2.identity);
+        System.out.println(m1.name + "," + "月薪" + m1.salary + ", id=" + m1.identity);
+        System.out.println(m2.name + "," + "月薪" + m2.salary + ", id=" + m2.identity);
     }
 }
--- java-projects/day4homework/src/p1/Member.java
+++
@@ -1,9 +0,0 @@
-package p1;
-
-public class Member {
-
-    String name;
-    int salary;
-    int identity;
-
-}
